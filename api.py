f213121ac58dfae3e07f36dcbdb5c5687de5528d
angelabtte <angel.bts12@gmail.com>
2025-05-12 12:33:52 -0500

    cuarto commit

--- api.py
+++ api.py
@@ -1,41 +1,54 @@
 import http.client
-import flask
-import util
-from calc import Calculator
+from flask import Flask
+from app import util
+from app.calc import Calculator
 
-api_application = flask.Flask(__name__)
 CALCULATOR = Calculator()
-HEADERS = {"Content-Type": "text/plain; charset=utf-8"}
+api_application = Flask(__name__)
+HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}
 
-@api_application.route("/", methods=["GET"])
-def index():
-    return ("Hello from The Calculator!\n", http.client.OK, HEADERS)
+
+@api_application.route("/")
+def hello():
+    return "Hello from The Calculator!\n"
+
 
 @api_application.route("/calc/add/<op_1>/<op_2>", methods=["GET"])
 def add(op_1, op_2):
     try:
-        op_1 = util.convert_to_number(op_1)
-        op_2 = util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.add(op_1, op_2)), http.client.OK, HEADERS)
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
 
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
 def substract(op_1, op_2):
     try:
-        op_1 = util.convert_to_number(op_1)
-        op_2 = util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.substract(op_1, op_2)), http.client.OK, HEADERS)
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-@api_application.route("/calc/sqrt/<number>", methods=["GET"])
-def sqrt(number):
+
+@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
+def multiply(op_1, op_2):
     try:
-        num = util.convert_to_number(number)
-        return ("{}".format(CALCULATOR.sqrt(num)), http.client.OK, HEADERS)
-    except (TypeError, ValueError) as e:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.multiply(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
+def divide(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        if num_2 == 0:
+            return ("Division by zero is not allowed.", http.client.NOT_ACCEPTABLE, HEADERS)
+        return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
 if __name__ == "__main__":
-    api_application.run(host="0.0.0.0")
+    api_application.run(host="0.0.0.0", port=5000)
--- app/calc.py
+++ app/calc.py
@@ -1,7 +1,9 @@
-import math
+import app
+
 
 class InvalidPermissions(Exception):
     pass
+
 
 class Calculator:
     def add(self, x, y):
@@ -19,25 +21,21 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise TypeError("Division by zero is not possible")
+            raise                                                TypeError("Division by zero is not possible")
+
         return x / y
 
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
 
-    def sqrt(self, x):
-        if not isinstance(x, (int, float)):
-            raise TypeError("Parameter must be a number")
-        if x < 0:
-            raise ValueError("Cannot calculate square root of a negative number")
-        return math.sqrt(x)
-
     def check_types(self, x, y):
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
+
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
     result = calc.add(2, 2)
     print(result)
+
